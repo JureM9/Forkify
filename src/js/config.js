f08a672259e5f0206afec86240508ec3fcb0d058
Wonderham92 <jure.mocnik9@gmail.com>
2023-03-15 17:17:30 +0100

    Initial comit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '69b7aa2f-ad66-4775-924f-4a7262a4da91';
+export const MODAL_CLOSE_SEC = 2.5;
